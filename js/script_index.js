c307b01de232392c21a287139ffd779543d803a1
Matheus <mathecarvalho2001@gmail.com>
2023-08-20 22:17:56 -0300

    Revert "Revert "V.14""
    This reverts commit 50be3b686a79747b98da3ae91bd033ff829e7565.

--- js/script_index.js
+++ js/script_index.js
@@ -1,3 +1,13 @@
 function niveis() {
     window.location = "./html/niveis.html";
 }
+
+function tutorial() {
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
+}
+
+function fecharD() {
+    const dialog = document.querySelector("dialog");
+    dialog.close();
+}
